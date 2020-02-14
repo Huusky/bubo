2a38cc2c140cf569324c5be6dc4eb3f6a841b71f
Huusky <22480762+Huusky@users.noreply.github.com>
2020-02-14 01:32:51 -0500

    query optimization for course

--- commands/course.js
+++ commands/course.js
@@ -8,12 +8,13 @@ module.exports = {
     enabled: true,
     async execute(message, args) {
         const course_id = args[0].toUpperCase();
-        const sql = `SELECT * FROM courses WHERE id=?`;
+        const sql = `SELECT name FROM courses WHERE id=?`;
         message.client.db.query(sql, [course_id], (err, result) => {
             if (err) {
                 logger.log(err, 'error');
                 return;
             }
+            console.log(result);
             message.reply(`${course_id} is ${result[0].name}`)
         });
     }
