4361d8ca22ca340b61ddcb36e64521c6bd6e767d
Huusky <22480762+Huusky@users.noreply.github.com>
2020-02-14 13:00:08 -0500

    basic help command finished

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'help',
     description: 'Get list of commands',
@@ -5,6 +7,27 @@ module.exports = {
     args: false,
     enabled: true,
     execute(message, args) {
-        
+        if (!args[0]) {
+            command_list = message.client.commands.map(command => command.name).join(', ');
+            const embed = new Discord.RichEmbed()
+                .setColor('#FFFFFF')
+                .setTitle('Help')
+                .addField('Available Commands', `${command_list}`)
+                .setTimestamp();
+            message.channel.send(embed);
+        }
+        if (args && message.client.commands.get(args[0])) {
+            const name = args[0].toLowerCase();
+            const command = message.client.commands.get(name);
+            const embed = new Discord.RichEmbed()
+                .setColor('#FFFFFF')
+                .setTitle(`${args[0].toLowerCase()} help`)
+                .addField('Name', `${command.name}`)
+                .addField('Description', `${command.description}`)
+                .addField('Usage', `${command.usage}`)
+                .addField('Args', `${command.args}`, true)
+                .addField('Enabled', `${command.enabled}`, true);
+            message.channel.send(embed);
+        }
     }
 }
