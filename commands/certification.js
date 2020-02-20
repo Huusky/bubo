d1436c23f9e20ec7155d9858b12988ea0fb1536f
Huusky <22480762+Huusky@users.noreply.github.com>
2020-02-20 12:29:31 -0500

    add certification command skeleton

---
+++ commands/certification.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'certification',
+    description: 'Get certification information',
+    usage: '<certification>',
+    args: true,
+    enabled: true,
+    async execute(message, args) {
+        
+    }
+}
