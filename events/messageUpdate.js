653d2b38c550f35546dc7e1df8e309c6c500a196
Alexander <22480762+Huusky@users.noreply.github.com>
2020-02-27 00:09:41 -0500

    fixed empty embed field issue

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,15 +2,17 @@ const { logChannel } = require('../config.json');
 const { RichEmbed } = require('discord.js');
 const logger = require('../util/logger');
 
-module.exports = async (client, oldMessage, newMessage) => {
+module.exports = (client, oldMessage, newMessage) => {
+    // If content is the same, done
+    if (oldMessage.content === newMessage.content) return;
     const embed = new RichEmbed()
-        .setAuthor(`${oldMessage.user.username}#${oldMessage.user.discriminator}`,
-            oldMessage.user.displayAvatarURL)
+        .setAuthor(`${oldMessage.author.tag}`, oldMessage.member.user.avatarURL)
         .setColor('#FFA500')
-        .setTitle(`Message edited in <#${oldMessage.channel.id}> [link](${oldMessage.url})`)
-        .addField('Message before', oldMessage.content)
-        .addField('Message after', newMessage.content)
-        .setFooter(`ID: ${oldMessage.user.id}`)
+        .setDescription(`**Message edited in **<#${oldMessage.channel.id}> [Jump to message](${oldMessage.url})`)
+        // Hacky shit here, \u200B for zero width space since discord complains about empty embed fields
+        .addField('Message before', `${oldMessage.content}\u200B`)
+        .addField('Message after', `${newMessage.content}\u200B`)
+        .setFooter(`User ID: ${oldMessage.author.id}`)
         .setTimestamp();
-    await client.channels.get(logChannel).send(embed);
+    client.channels.get(logChannel).send(embed);
 }
