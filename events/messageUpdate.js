c5b1b72110bffbf5c482e61bc725f4ccc90864ab
Alexander <22480762+Huusky@users.noreply.github.com>
2020-02-26 22:21:33 -0500

    message update audit

---
+++ events/messageUpdate.js
@@ -0,0 +1,16 @@
+const { logChannel } = require('../config.json');
+const { RichEmbed } = require('discord.js');
+const logger = require('../util/logger');
+
+module.exports = (client, oldMessage, newMessage) => {
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`${oldMessage.user.username}#${oldMessage.user.discriminator}`,
+            oldMessage.user.displayAvatarURL)
+        .setColor('#FFA500')
+        .setTitle(`Message edited in <#${oldMessage.channel.id}> [link](${oldMessage.url})`)
+        .addField('Message before', oldMessage.content)
+        .addField('Message after', newMessage.content)
+        .setFooter(`ID: ${oldMessage.user.id}`)
+        .setTimestamp();
+await client.channels.get(logChannel).send(embed);
+}
