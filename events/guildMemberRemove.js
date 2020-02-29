0b3529de55b69e278d8ca42fa13b006e7bae80b0
Alexander <22480762+Huusky@users.noreply.github.com>
2020-02-29 14:12:08 -0500

    add color to member join/leave

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -5,6 +5,7 @@ const logger = require('../util/logger');
 module.exports = async (client, member) => {
     const embed = new Discord.RichEmbed()
         .setAuthor('Member Left', member.user.displayAvatarURL)
+        .setColor('#963232')
         .setDescription(`<@${member.id}> ${member.user.username}#${member.user.discriminator}`)
         .setFooter(`ID: ${member.id}`);
     await client.channels.get(logChannel).send(embed);
