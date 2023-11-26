f319fbe5e7434586a87eb49a253f1593c13f0938
smcgb <smcgb1844@gmail.com>
2023-11-25 16:04:02 -0800

    Feature: Removed alumnUdemy added DNATA

--- src/Commands/Info/Dnata.ts
+++ src/Commands/Info/Dnata.ts
@@ -5,12 +5,12 @@ import SlashCommand from "../../Structures/SlashCommand";
 export default class extends SlashCommand {
     public constructor() {
         super({
-            name: 'udemyAlumni',
-            description: 'Get a link to Alumni Udemy',
+            name: "Don't ask to ask",
+            description: 'post DNATA',
         });
     }
 
     public async run(interaction: CommandInteraction, client: Client): Promise<any> {
-        return await interaction.reply('wgualumni.udemy.com/');
+        return await interaction.reply("Don't ask to ask, just ask \n https://dontasktoask.com/");
     }
 }
